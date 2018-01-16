b83339409e7a0eecdda74fa6bf6aa99b09ea14c3
Paperpal9 <35234901+Paperpal9@users.noreply.github.com>
2018-01-15 20:56:33 -0500

    Add files via upload

--- Weapon.java
+++ Weapon.java
@@ -64,6 +64,70 @@ public class Weapon {
        dex = 5;
        price = 1000;
        break;
+     case 7:
+       name = "Short Weapon";
+       adjective = "Short";
+       str = 0;
+       con = 0;
+       agi = 1;
+       dex = -1;
+       price = 5;
+       break;
+     case 8:
+       name = "Weighted Weapon";
+       adjective = "Weighted";
+       str = 2;
+       con = 1;
+       agi = -2;
+       dex = 0;
+       price = 10;
+       break;
+     case 9:
+       name = "Long Weapon";
+       adjective = "Long";
+       str = 1;
+       con = 0;
+       agi = 0;
+       dex = 3;
+       price = 25;
+       break;
+     case 10:
+       name = "Living Weapon";
+       adjective = "Living";
+       str = 3;
+       con = 1;
+       agi = 2;
+       dex = 3;
+       price = 100;
+       break;
+     case 11:
+       name = "Energized Weapon";
+       adjective = "Energized";
+       str = 4;
+       con = 3;
+       agi = 3;
+       dex = 3;
+       price = 500;
+       break;
+     case 12:
+       name = "Old Weapon";
+       adjective = "Old";
+       str = -1;
+       con = 0;
+       agi = 0;
+       dex = 0;
+       price = 0;
+       break;
+     case 13:
+       name = "Dirty Weapon";
+       adjective = "Dirty";
+       str = -5;
+       con = -5;
+       agi = -5;
+       dex = -5;
+       price = 0;
+       break;
+       
    }
   }
  
